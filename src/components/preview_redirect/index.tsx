9bd9597163b483312e48f7d09f7da5925bd3f1c3
Freddie Wang <tsfreddiewang@gmail.com>
2021-03-22 13:07:22 +0800

    stuff

--- src/components/preview_redirect/index.tsx
+++ src/components/preview_redirect/index.tsx
@@ -17,6 +17,8 @@ interface Attr {
 type CVnode = m.CVnode<Attr>;
 
 export default class implements m.ClassComponent<Attr> {
+  map: string = null;
+  init: boolean;
   isWeird: boolean;
   targetUrl: string;
 
@@ -25,91 +27,99 @@ export default class implements m.ClassComponent<Attr> {
     this.targetUrl = url;
   };
   constructor({ attrs: { map } }: CVnode) {
+    this.map = map;
+    this.init = false;
     document.title = `地图预览 - Teeworlds中文社区`;
+  }
+
+  oncreate() {
     this.isWeird = /Mobi/i.test(navigator.userAgent) && window.innerWidth <= window.outerWidth;
     this.redirect(
       `https://teeworlds.cn/mappreview/?map=https://api.teeworlds.cn/ddnet/mapdata/${encodeURIComponent(
-        map
+        this.map
       )}`,
       !this.isWeird
     );
+    this.init = true;
   }
 
   view() {
     return (
       <div class="section container">
-        <div class="columns is-desktop">
-          <div class="column is-half is-offset-3">
-            {!this.isWeird ? (
-              <div class="card">
-                <header class="card-header">
-                  <p class="card-header-title">地图预览</p>
-                </header>
-                <div class="card-content">
-                  <div class="field">
-                    <label class="label">
-                      正在为您跳转到地图预览工具， 若没有自动跳转，请点击下面的按钮:
-                    </label>
+        {this.init && (
+          <div class="columns is-desktop">
+            <div class="column is-half is-offset-3">
+              {!this.isWeird ? (
+                <div class="card">
+                  <header class="card-header">
+                    <p class="card-header-title">地图预览</p>
+                  </header>
+                  <div class="card-content">
+                    <div class="field">
+                      <label class="label">
+                        正在为您跳转到地图预览工具， 若没有自动跳转，请点击下面的按钮:
+                      </label>
+                      <div class="control">
+                        <a class="button is-info" href={this.targetUrl}>
+                          <span class="icon">
+                            <i class="fas fa-directions"></i>
+                          </span>
+                          <span>立即跳转</span>
+                        </a>
+                      </div>
+                    </div>
+                    <progress class="progress is-primary" max="100"></progress>
+                  </div>
+                </div>
+              ) : (
+                <div class="card">
+                  <header class="card-header">
+                    <p class="card-header-title">地图预览</p>
+                  </header>
+                  <div class="card-content">
+                    <div class="field">
+                      <label class="label">
+                        某些应用内暂时不支持地图预览，请手动复制链接并用浏览器打开：
+                      </label>
+                      <div class="control"></div>
+                      <textarea class="textarea" readonly="true" rows="7">
+                        {this.targetUrl}
+                      </textarea>
+                    </div>
                     <div class="control">
-                      <a class="button is-info" href={this.targetUrl}>
+                      <button
+                        class="button is-info"
+                        onclick={() =>
+                          clipboard.writeText(this.targetUrl).then(
+                            () => {
+                              bulmaToast.toast({
+                                message: '地址已复制',
+                                type: 'is-success',
+                                dismissible: true,
+                              });
+                            },
+                            () => {
+                              bulmaToast.toast({
+                                message: '地址复制失败',
+                                type: 'is-danger',
+                                dismissible: true,
+                              });
+                            }
+                          )
+                        }
+                      >
                         <span class="icon">
-                          <i class="fas fa-directions"></i>
+                          <i class="fas fa-clipboard"></i>
                         </span>
-                        <span>立即跳转</span>
-                      </a>
+                        <span>复制链接</span>
+                      </button>
                     </div>
                   </div>
-                  <progress class="progress is-primary" max="100"></progress>
                 </div>
-              </div>
-            ) : (
-              <div class="card">
-                <header class="card-header">
-                  <p class="card-header-title">地图预览</p>
-                </header>
-                <div class="card-content">
-                  <div class="field">
-                    <label class="label">
-                      某些应用内暂时不支持地图预览，请手动复制链接并用浏览器打开：
-                    </label>
-                    <div class="control"></div>
-                    <textarea class="textarea" readonly="true" rows="7">
-                      {this.targetUrl}
-                    </textarea>
-                  </div>
-                  <div class="control">
-                    <button
-                      class="button is-info"
-                      onclick={() =>
-                        clipboard.writeText(this.targetUrl).then(
-                          () => {
-                            bulmaToast.toast({
-                              message: '地址已复制',
-                              type: 'is-success',
-                              dismissible: true,
-                            });
-                          },
-                          () => {
-                            bulmaToast.toast({
-                              message: '地址复制失败',
-                              type: 'is-danger',
-                              dismissible: true,
-                            });
-                          }
-                        )
-                      }
-                    >
-                      <span class="icon">
-                        <i class="fas fa-clipboard"></i>
-                      </span>
-                      <span>复制链接</span>
-                    </button>
-                  </div>
-                </div>
-              </div>
-            )}
+              )}
+            </div>
           </div>
-        </div>
+        )}
       </div>
     );
   }
