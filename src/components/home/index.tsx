13b01d3bb1616d66c4cc6e2f09fc6f60d8c5972a
Freddie Kins <tsfreddiewang@gmail.com>
2021-08-05 15:04:34 +0800

    audit and use m.route.Link

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -15,28 +15,30 @@ export default class implements m.ClassComponent {
         <div class="content">
           <section class="section is-large">
             <div class="serverlist">
-              <a class="button" href="/browser">
+              {m(m.route.Link, {href: "/browser", class: "button"},
                 <h1 class="title">服务器浏览器</h1>
-              </a>
-              <h2 class="subtitle">
-                搜索 Teeworlds 在线服务器！
-              </h2>
+              )}
+              <h2 class="subtitle">搜索 Teeworlds 在线服务器！</h2>
             </div>
             <div class="wiki">
               <a class="button" href="https://wiki.teeworlds.cn/">
                 <h1 class="title">中文维基百科</h1>
               </a>
-              <h2 class="subtitle">
-                原生模式/mod介绍，服务器开设指南，闯关指南...
-              </h2>
+              <h2 class="subtitle">原生模式/mod介绍，服务器开设指南，闯关指南...</h2>
             </div>
           </section>
           <section class="section is-medium">
             <div class="link">
               <h1 class="title">中文社区</h1>
-              <a class="button" href="https://www.kaiheila.cn/app/invite/pNXyP8">开黑啦</a>
-              <a class="button" href="https://discord.gg/dqwuHEq">Discord</a>
-              <a class="button" href="https://t.me/teeworldscn">Telegram</a>
+              <a class="button" href="https://www.kaiheila.cn/app/invite/pNXyP8">
+                开黑啦
+              </a>
+              <a class="button" href="https://discord.gg/dqwuHEq">
+                Discord
+              </a>
+              <a class="button" href="https://t.me/teeworldscn">
+                Telegram
+              </a>
             </div>
           </section>
         </div>
